18b7320c83f854b8f44ec31eecbba444f2a801ab
Khue Doan <mail@khuedoan.com>
2024-05-20 00:50:02 +0700

    feat: add dev box

---
+++ staging/devbox.tf
@@ -0,0 +1,48 @@
+resource "proxmox_virtual_environment_container" "devbox" {
+  description = "Development instance, can be used as a bastion host"
+  node_name   = "proxmox"
+
+  cpu {
+    cores = 4
+  }
+
+  memory {
+    dedicated = 1024 * 4
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/nixos-devbox.tar.xz"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 32
+  }
+
+  network_interface {
+    name = "veth0"
+  }
+
+  initialization {
+    hostname = "devbox"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  features {
+    nesting = true
+  }
+
+  console {
+    type = "console"
+  }
+
+  tags = [
+    "homecloud",
+    "staging"
+  ]
+}
