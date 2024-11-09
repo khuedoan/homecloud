3c4eb669a129915b55bf34e4b267e50ab2954d76
Khue Doan <mail@khuedoan.com>
2024-11-09 21:54:45 +0700

    feat(incus): add preseed config

--- metal/incus.nix
+++ metal/incus.nix
@@ -19,6 +19,47 @@
       ui = {
         enable = true;
       };
+      preseed = {
+        config = {
+          "core.https_address" = "[::]:8443";
+        };
+        storage_pools = [
+          {
+            name = "default";
+            driver = "btrfs";
+            config = {
+              size = "35GiB";
+            };
+          }
+        ];
+        networks = [
+          {
+            name = "incusbr0";
+            type = "bridge";
+            config = {
+              "ipv4.address" = "auto";
+              "ipv6.address" = "auto";
+            };
+          }
+        ];
+        profiles = [
+          {
+            name = "default";
+            devices = {
+              eth0 = {
+                name = "eth0";
+                network = "incusbr0";
+                type = "nic";
+              };
+              root = {
+                path = "/";
+                pool = "default";
+                type = "disk";
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
