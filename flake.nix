68c05e4fee08067ff7d59991ddfa0463cfbb1584
Khue Doan <mail@khuedoan.com>
2025-01-01 00:52:28 +0700

    chore: upgrade to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
