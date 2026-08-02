649bf00064fb84752cd8b35097b9177240c13d0c
Khue Doan <mail@khuedoan.com>
2026-08-02 17:30:19 +0700

    feat: nginx and certs for kanidm and incus

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 
+let
+  domain = "khuedoan.com";
+  cloudHost = "cloud.${domain}";
+  idHost = "id.${cloudHost}";
+  cloudUrl = "https://${cloudHost}";
+  idUrl = "https://${idHost}";
+in
 {
   disko.devices = {
     disk = {
@@ -101,16 +108,51 @@
     fail2ban = {
       enable = true;
     };
+    nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+      appendHttpConfig = ''
+        proxy_ssl_server_name on;
+        proxy_ssl_name $server_name;
+        proxy_ssl_trusted_certificate ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt;
+        proxy_ssl_verify on;
+      '';
+      virtualHosts = {
+        "${cloudHost}" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "https://127.0.0.1:8443";
+            proxyWebsockets = true;
+            extraConfig = ''
+              client_max_body_size 0;
+              proxy_buffering off;
+              proxy_request_buffering off;
+              proxy_read_timeout 3600s;
+            '';
+          };
+        };
+        "${idHost}" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "https://127.0.0.1:8444";
+            proxyWebsockets = true;
+          };
+        };
+      };
+    };
     kanidm = {
       package = pkgs.kanidm_1_10;
       server = {
         enable = true;
         settings = {
           bindaddress = "127.0.0.1:8444";
-          domain = "id.cloud.khuedoan.com";
-          origin = "https://id.cloud.khuedoan.com";
-          tls_chain = "/var/lib/acme/id.cloud.khuedoan.com/fullchain.pem";
-          tls_key = "/var/lib/acme/id.cloud.khuedoan.com/key.pem";
+          domain = idHost;
+          origin = idUrl;
+          tls_chain = "/var/lib/acme/${idHost}/fullchain.pem";
+          tls_key = "/var/lib/acme/${idHost}/key.pem";
           online_backup = {
             path = "/var/lib/kanidm/backups";
             schedule = "00 22 * * *";
@@ -121,7 +163,7 @@
       client = {
         enable = true;
         settings = {
-          uri = "https://id.cloud.khuedoan.com";
+          uri = idUrl;
           verify_ca = true;
           verify_hostnames = true;
         };
@@ -140,8 +182,8 @@
         systems.oauth2.incus = {
           displayName = "Incus";
           public = true;
-          originUrl = "https://cloud.khuedoan.com/oidc/callback";
-          originLanding = "https://cloud.khuedoan.com/ui/";
+          originUrl = "${cloudUrl}/oidc/callback";
+          originLanding = "${cloudUrl}/ui/";
           scopeMaps.incus-users = [
             "openid"
             "profile"
@@ -164,15 +206,37 @@
     };
   };
 
+  security = {
+    acme = {
+      acceptTerms = true;
+      certs = {
+        "${idHost}" = {
+          group = "kanidm";
+          reloadServices = [ "kanidm.service" ];
+        };
+      };
+    };
+  };
+
+  systemd.services.kanidm = {
+    after = [ "acme-${idHost}.service" ];
+    requires = [ "acme-${idHost}.service" ];
+  };
+
   virtualisation = {
     incus = {
       enable = true;
+      useACMEHost = cloudHost;
       ui = {
         enable = true;
       };
       preseed = {
         config = {
           "core.https_address" = ":8443";
+          "oidc.audience" = "incus";
+          "oidc.client.id" = "incus";
+          "oidc.issuer" = "${idUrl}/oauth2/openid/incus";
+          "oidc.scopes" = "openid,profile,email";
           "user.ui.title" = "HomeCloud";
         };
         networks = [
@@ -217,6 +281,9 @@
   };
 
   users.users = {
+    nginx = {
+      extraGroups = [ "kanidm" ];
+    };
     root = {
       # TODO better way to SSH, maybe without SSH key
       openssh.authorizedKeys.keys = [
